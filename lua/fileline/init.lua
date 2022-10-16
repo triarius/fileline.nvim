3e45f8055bac53f0617956d477d4b7df6510b3ba
Narthana Epa <narthana.epa@gmail.com>
2022-10-17 04:47:07 +1100

    Rename module

--- lua/fileline/init.lua
+++ lua/fileline/init.lua
@@ -1,10 +1,10 @@
-local function get_fileline()
+local M = {}
+
+M.get_fileline = function()
 	local filename = vim.fn.expand("%:~:.")
 	local l, c = unpack(vim.api.get_win_cursor)
 	local fileline = filename .. ":" .. l .. ":" .. c
 	return fileline
 end
 
-return {
-	get_fileline = get_fileline,
-}
+return M
