2b654d41e18e9dfb0f86385145ccdcf47ce76385
Narthana Epa <narthana.epa@gmail.com>
2022-10-18 00:12:07 +1100

    Add function to parse file name

--- lua/fileline/init.lua
+++ lua/fileline/init.lua
@@ -7,4 +7,32 @@ M.get_fileline = function()
 	return fileline
 end
 
+M.parse_filename = function(filename)
+	local f, l, c = string.match(filename, "^(.*):(%d+):(%d+)$")
+
+	if f ~= nil then
+		return {
+			filename = f,
+			line = tonumber(l),
+			column = tonumber(c),
+		}
+	end
+
+	f, l = string.match(filename, "^(.*):(%d+)$")
+
+	if f ~= nil then
+		return {
+			filename = f,
+			line = tonumber(l),
+			column = -1,
+		}
+	end
+
+	return {
+		filename = f,
+		line = tonumber(l),
+		column = -1,
+	}
+end
+
 return M
