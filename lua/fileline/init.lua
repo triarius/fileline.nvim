50add210b7112ad6abc1cec248c91b2642bc87b5
Narthana Epa <narthana.epa@gmail.com>
2022-10-18 00:12:47 +1100

    Remove unnecessary variable

--- lua/fileline/init.lua
+++ lua/fileline/init.lua
@@ -3,8 +3,7 @@ local M = {}
 M.get_fileline = function()
 	local filename = vim.fn.expand("%:~:.")
 	local l, c = unpack(vim.api.nvim_win_get_cursor(0))
-	local fileline = filename .. ":" .. l .. ":" .. c
-	return fileline
+	return filename .. ":" .. l .. ":" .. c
 end
 
 M.parse_filename = function(filename)
