1b9a551d3b40126f1ac3f7614dda9d159df0590e
Narthana Epa <narthana.epa@gmail.com>
2022-10-19 23:24:17 +1100

    Remove useless print

--- lua/fileline/init.lua
+++ lua/fileline/init.lua
@@ -3,7 +3,6 @@ local construct = require("fileline.construct")
 local M = {}
 
 function M.setup(args)
-	print(args)
 	vim.api.nvim_create_user_command("Fileline", function()
 		local fileline = construct.fileline()
 		vim.fn.setreg("+", fileline)
