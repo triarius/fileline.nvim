d6eceb189b83f8f42dd6f8b889d14fd2db6ccf70
Narthana Epa <narthana.epa@gmail.com>
2022-10-19 23:24:53 +1100

    Add option for destination register

--- lua/fileline/init.lua
+++ lua/fileline/init.lua
@@ -3,9 +3,11 @@ local construct = require("fileline.construct")
 local M = {}
 
 function M.setup(args)
+	local destination_register = args["destination_register"] or "+"
+
 	vim.api.nvim_create_user_command("Fileline", function()
 		local fileline = construct.fileline()
-		vim.fn.setreg("+", fileline)
+		vim.fn.setreg(destination_register, fileline)
 	end, {})
 end
 
