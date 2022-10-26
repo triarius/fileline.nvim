045f5303b3bfe38b764378b48e126a603ecb82f8
Narthana Epa <narthana.epa@gmail.com>
2022-10-27 00:32:41 +1100

    Add original filename to parsed output

--- lua/fileline/parse.lua
+++ lua/fileline/parse.lua
@@ -1,10 +1,11 @@
 -- returns a table with the keys
 -- filename = the name of the file
--- line = the line number (if any) or -1
--- column = the column number (if any) or -1
+-- line = the line number (if any)
+-- column = the column number (if any)
 return function(filename)
 	local matches = vim.fn.matchlist(filename, [[\v^(.{-})(:(\d+))?(:(\d+))?$]])
 	return {
+		original_filename = filename,
 		filename = matches[2],
 		line = tonumber(matches[4]),
 		column = tonumber(matches[6]),
