a5718316c52167fb80274e5fc47ff4dc355c0663
Narthana Epa <narthana.epa@gmail.com>
2022-11-16 09:26:10 +1100

    refactor(fileline): abstract string building from position finding

--- lua/fileline/construct.lua
+++ lua/fileline/construct.lua
@@ -1,5 +1,9 @@
+local function standard(filename, line, column)
+	return filename .. ":" .. line .. ":" .. column
+end
+
 return function()
 	local filename = vim.fn.expand("%:~:.")
-	local l, c = unpack(vim.api.nvim_win_get_cursor(0))
-	return filename .. ":" .. l .. ":" .. c
+	local line, column = unpack(vim.api.nvim_win_get_cursor(0))
+	return standard(filename, line, column)
 end
